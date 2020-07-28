15825930ad2877b129da071e85f9b347d81b6bac
EdS5 <46868845+EdS5@users.noreply.github.com>
2020-07-28 21:33:03 +0300

    Add files via upload

---
+++ cogs/giveaway.py
@@ -0,0 +1,76 @@
+import discord
+from discord.ext import commands
+import time
+import random
+from asyncio import sleep
+from discord.ext.commands import errors
+
+
+class Giveaway(commands.Cog):
+    def __init__(self, bot):
+        self.client = bot
+
+    @commands.command(name="giveaway")
+    async def giveaway(self, ctx, arg1=None, arg2 : discord.TextChannel=None, *, args=None):
+        try:
+            if ctx.author.guild_permissions.administrator is True:
+                if arg1 is None or arg2 is None:
+                    argEmbed = discord.Embed(title="Ошибка", description="Не были введены аргументы")
+                    await ctx.send(embed=argEmbed)
+                if arg1 is not None and arg2 is not None and args is None:
+                    argEmbed = discord.Embed(title="Ошибка", description="Не были введены аргументы")
+                    await ctx.send(embed=argEmbed)
+                else:
+                    CorrectlyEmbed = discord.Embed(title="Розыгрыш создан!", color=discord.Color.blue(),
+                                                  description=f"**Время розыгрыша:** {arg1} секунд\n**Канал сообщения:** <#{arg2}>\n**Приз:** {args}")
+                    await ctx.message.delete(delay=1.7)
+                    await ctx.send(embed=CorrectlyEmbed, delete_after=15.0)
+
+                    channel = self.client.get_channel(arg2.id)
+                    newembed = discord.Embed(title=":tada: РОЗЫГРЫШ :tada:",
+                                            description=f"**Нажмите на реакцию, чтобы участвовать.**\n\n**Приз:** {args}\n**Время розыгрыша:** {arg1} секунд")
+                    message = await channel.send(embed=newembed)
+                    giveawayEmoji = self.client.get_emoji(731079730203459615)
+                    await message.add_reaction(giveawayEmoji)
+
+
+                    await sleep(int(arg1))
+
+                    message_new = await channel.fetch_message(message.id)
+
+                    giveawayReaction = []
+                    for reaction in message_new.reactions:
+                        if reaction.emoji == giveawayEmoji:
+                            giveawayReaction = reaction
+
+                    who_reacted = list(await giveawayReaction.users().flatten())
+                    user_bot = discord.utils.find(lambda m: m.id == 637750757319245835, ctx.guild.members)
+                    who_reacted.remove(user_bot)
+                    winner = random.choice(who_reacted)
+                    EmWin = discord.Embed(title="<a:dConrats:731079711815630901> ПОБЕДИТЕЛЬ <a:dConrats:731079711815630901>", color=discord.Color.blue(), description=f"{winner.mention} выиграл **{args}**!")
+                    await channel.send(embed=EmWin)
+
+            else:
+                embeder = discord.Embed(title="Ошибка", color=discord.Color.red(),
+                                        description="У вас недостаточно прав")
+                await ctx.message.delete(delay=1.0)
+                await ctx.send(embed=embeder, delete_after=5.0)
+
+        except ValueError:
+            emer = discord.Embed(title="Ошибка", color=discord.Color.red(), description="Неправильный тип аргументов")
+            await ctx.message.delete(delay=1.0)
+            await ctx.send(embed=emer, delete_after=5.0)
+        except TypeError:
+            pass
+
+    @giveaway.error
+    async def giveaway_error(self, ctx, error):
+        if isinstance(error, commands.BadArgument):
+            emerr = discord.Embed(title="Ошибка", color=discord.Color.red(),
+                                  description='Такого канала нету на данном сервере')
+            await ctx.send(embed=emerr)
+
+
+
+def setup(bot):
+    bot.add_cog(Giveaway(bot))
--- main.py
+++ main.py
@@ -1,16 +1,48 @@
 import discord
+from discord.ext import commands
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as', self.user)
 
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
 
-        if message.content == 'ping':
-            await message.channel.send('pong')
+client = commands.Bot(command_prefix = '!')
+TOKEN = 'NjM3NzUwNzU3MzE5MjQ1ODM1.XbStrQ._qLJAV3c0xin7BVTGSOz3o5YmvI'
+client.remove_command("help")
 
-client = MyClient()
-client.run('NjM3NzUwNzU3MzE5MjQ1ODM1.XbSttA.FwgfcNENFVZZognRtynzzPeoArI')
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.online, activity=discord.Activity(name="version 0.3 | !help", url="https://www.twitch.tv/eds52", type=discord.ActivityType.watching))
+    print("Bot is ready.")
+
+
+#@client.command(pass_context=True)
+#async def case(ctx, arg1=None, arg2=None):
+ #   if arg1 == 'open' and arg2 == 'dlore':
+#
+ #       responses = ['awp басков',
+  #                   'егор криг',
+   #                  'калаш малахов',
+    #                 'дигл пугачёва']
+     #   await ctx.send(f'Вам выпало - ***{random.choice(responses)}***')
+    #if arg1 is None:
+     #   embedNoArg = discord.Embed(title="Ошибка",description="Не были введены аргументы")
+      #  await ctx.send(embed=embedNoArg)
+   # if arg1 == "open" and arg2 is None:
+    #    embedSelectCase = discord.Embed(title="Выберите кейс из доступных")
+     #   embedSelectCase.add_field(name="dlore",value="Не включает ничего. буквально")
+      #  await ctx.send(embed=embedSelectCase)
+   # else:
+    #    pass
+
+
+
+
+
+cogs = {"information", "8ball", "giveaway", "avatar", "moderation", "other"}
+
+for cog in cogs:
+    client.load_extension(f"cogs.{cog}")
+
+
+
+client.run(TOKEN)
